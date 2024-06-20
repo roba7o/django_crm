e9d2185687046e40848f3a3c5eb24483c8397dcf
roba7o <rhm14148@gmail.com>
2024-06-20 09:36:09 +0100

    Authenticated user

--- dcrm/website/views.py
+++ dcrm/website/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .forms import SignUpForm
 
 # Create your views here.
 
@@ -29,3 +30,21 @@ def logout_user(request):
     logout(request)
     messages.success(request, "you have been logged out mate")
     return redirect('home')
+
+def register_user(request):
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            #Authenticate and login
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, "You have successfully logged in ")
+            return redirect('home')
+    else:
+        form = SignUpForm()
+        return render(request, 'website/register.html', {'form':form}) 
+        
+    return render(request, 'website/register.html', {'form':form}) 
